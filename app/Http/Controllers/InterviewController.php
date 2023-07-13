2bd04e6c9091d2e80776a98f833a960c2263fe5d
Bagus Farhan Kirana <bagusfk08@gmail.com>
2023-07-13 16:43:22 +0700

    all relationship is done

---
+++ app/Http/Controllers/InterviewController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Interview;
+use Illuminate\Http\Request;
+
+class InterviewController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $interview = Interview::create([
+            'application_id' => $request->input('application_id'),
+            'user_id' => $request->input('user_id'),
+            'type' => $request->input('type'),
+            'deskripsi' => $request->input('deskripsi'),
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Interview $interview)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Interview $interview)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Interview $interview)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Interview $interview)
+    {
+        //
+    }
+}
--- app/Models/Lamaran.php
+++ app/Models/Lamaran.php
@@ -29,4 +29,9 @@ class Lamaran extends Model
     {
         return $this->hasOne(Answer::class);
     }
+
+    public function interview()
+    {
+        return $this->hasMany(Interview::class);
+    }
 }
