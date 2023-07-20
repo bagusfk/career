d80737e1227484bf49a658720b9f4e5f7625bf06
Bagus Farhan Kirana <bagusfk08@gmail.com>
2023-07-20 08:36:48 +0700

    component

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -28,11 +28,9 @@
     <link href="./assets/css/argon-dashboard-tailwind.css?v=1.0.1" rel="stylesheet"/>
 </head>
 
-<body
-    class="m-0 font-sans text-base antialiased font-normal dark:bg-slate-900 leading-default bg-gray-50 text-slate-500">
+<body class="m-0 font-sans text-base antialiased font-normal dark:bg-slate-900 leading-default bg-gray-100 text-slate-500">
 <div class="absolute w-full bg-blue-500 dark:hidden min-h-75"></div>
 @include('layouts.sidenav')
-
 <main class="relative h-full max-h-screen transition-all duration-200 ease-in-out xl:ml-68 rounded-xl">
 
     <!-- Navbar -->
