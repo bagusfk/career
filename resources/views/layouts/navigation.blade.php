ce10ce048d742521e9d247ffb3f599f01c1e0888
Bagus Farhan Kirana <bagusfk08@gmail.com>
2023-07-21 01:41:21 +0700

    ui landing page

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,16 +1,20 @@
-<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+<nav x-data="{ open: false }" class="bg-white h-24 dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
+        <div class="flex justify-between h-24 -mt-1">
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('user.index') }}">
+                    <a href="{{ route('user.index') }}" class="flex items-center">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <div class="text-lg font-bold ml-2 dark:text-gray-100">
+                            WPS Career
+                        </div>
                     </a>
+                </div>
+                <div class="shrink-0 flex items-center sm:-my-px sm:ml-5 sm:flex">
                     <x-toogle/>
                 </div>
-
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('user.index')" :active="request()->routeIs('user.index')">
@@ -32,6 +36,10 @@
                     @endif
                     @if (Auth::user()->role === 'admin')
                         <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                            <x-nav-link :href="route('admin.index')" :active="request()->routeIs('admin.index')">
+                                {{ __('Dashboard') }}
+                            </x-nav-link>
+                        </div><div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                             <x-nav-link :href="route('lowongan.index')" :active="request()->routeIs('lowongan.index')">
                                 {{ __('Kelola Lowongan') }}
                             </x-nav-link>
@@ -47,7 +55,6 @@
                             </x-nav-link>
                         </div>
                     @endif
-                    {{-- @if (Auth::user()->role === 'interviewer'||Auth::user()->role ==='admin') --}}
                     @if (in_array(Auth::user()->hasRole(), ['interviewer','admin']))
                         <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                             <x-nav-link :href="route('interviewer.index')" :active="request()->routeIs('interviewer.index')">
